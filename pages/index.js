7343cc83c32ed6dd4960d9c399696abd6f0b92ec
AliShahbaz <alishahbazbal1234@gmail.com>
2023-07-18 23:28:08 +0500

    Revert "Adding buy me a blunt option"
    This reverts commit 0b8be370d6d645583974eca41963d1fcfad81dd4.

--- pages/index.js
+++ pages/index.js
@@ -5,32 +5,13 @@ import Header from '../components/Header'
 import Feed from '../components/Feed'
 import RightSidebar from '../components/RightSidebar'
 import Sidebar from '../components/Sidebar'
-import Image from 'next/image'
-import blunt from '../assets/blunt.png'
-import Blunt from '../components/Blunt'
-import styled from 'styled-components';
-
 
 const style = {
   wrapper: `bg-[#18191a] min-h-screen duration-[0.5s]`,
   homeWrapper: `flex`,
   center: `flex-1`,
   main: `flex-1 flex justify-center`,
-  signupContainer: `flex items-center justify-center w-screen h-[60vh]`,
-  floatingImage: `
-  position: fixed;
-  left: 0;
-  bottom: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: #000;
-  color: #fff;
-  padding: 10px;
-  border-radius: 5px;
-  cursor: pointer;
-  z-index: 999;
-`,
+  signupContainer: `flex items-center justify-center w-screen h-[70vh]`,
 }
 
 export default function Home() {
@@ -58,25 +39,10 @@ export default function Home() {
     }
   }
 
-  const CenteredContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  // width: 100vh;
-`;
-
-
-  const handleClick = () => {
-    console.log('hello')
-  }
-
   return (
     <div className={style.wrapper}  >
       <Header name={name} url={url}  />
 
-      <CenteredContainer>
-      <Blunt></Blunt>
-      </CenteredContainer>
       {registered ? (
         <div className={style.homeWrapper} >
           {/* <Sidebar name={name} url={url} /> */}
@@ -103,21 +69,6 @@ export default function Home() {
           />
         </div>
       )}
-
-{/* <div className={style.floatingImage} onClick={handleClick}>
-  <div style={{marginLeft:'10px'}}>
-  <Image
-    src={blunt}
-    alt=''
-    height={30}
-    width={30}
-    style={{ marginLeft: '10px' }} // Add left padding here
-  />
-    <span style={{ color: 'white', marginLeft: '10px' }}>Buy me a blunt</span>
-  </div> */}
-
-{/* </div> */}
-
     </div>
   )
 }
