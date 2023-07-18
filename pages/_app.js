0b8be370d6d645583974eca41963d1fcfad81dd4
AliShahbaz <alishahbazbal1234@gmail.com>
2023-07-18 23:20:40 +0500

    Adding buy me a blunt option

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import dynamic from 'next/dynamic'
 import '../styles/globals.css'
 import { WalletBalanceProvider } from '../context/useWalletBalance'
 import { ModalProvider } from 'react-simple-hook-modal'
+import { useEffect } from 'react';
 
 const WalletConnectionProvider = dynamic(
   () => import('../context/WalletConnectionProvider'),
@@ -11,6 +12,16 @@ const WalletConnectionProvider = dynamic(
 )
 
 function MyApp({ Component, pageProps }) {
+
+
+  useEffect(() => {
+    // Set the background color for the whole app
+    document.body.classList.add('bg-[#18191a]');
+    return () => {
+      document.body.classList.remove('bg-[#18191a]');
+    };
+  }, []);
+
   return (
     <WalletConnectionProvider>
       <WalletBalanceProvider>
